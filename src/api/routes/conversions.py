8921905a19712591f2cdc42a0f172bd8d785fe41
Chase Roohms <chaseroohms@gmail.com>
2026-02-17 22:11:01 -0600

    feat: super rough implementation of the conversions api endpoint

--- src/api/routes/conversions.py
+++ src/api/routes/conversions.py
@@ -1,8 +1,26 @@
-from fastapi import APIRouter
+import json
+from fastapi import APIRouter, Request
+from converters import ConverterInterface
+from registry import ConverterRegistry
 
 router = APIRouter(prefix="/conversions", tags=["conversions"])
-
+regisitry = ConverterRegistry()
 
 @router.get("/")
 def list_conversions():
     return {"conversions": []}
+
+@router.post("/")
+async def create_conversion(request: Request):
+    body = await request.json()
+    id = body.get("id")
+    input_format = body.get("input_format")
+    output_format = body.get("output_format")
+    print(f"Received conversion request: id={id}, input_format={input_format}, output_format={output_format}")
+    converter_type = regisitry.get_converter_for_conversion(input_format, output_format)
+    if converter_type is None:
+        return {"error": f"No converter found for {input_format} to {output_format}"}
+    
+    converter: ConverterInterface = converter_type(f'uploads/{id}.{input_format}', f'converted/', input_format, output_format)
+    print(converter.convert())
+    return {"message": "Conversion created"}
