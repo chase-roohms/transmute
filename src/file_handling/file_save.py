ae96c5979ff784183ecf2153398e40486334d12b
Chase Roohms <chaseroohms@gmail.com>
2026-02-18 12:50:08 -0600

    refactor: restructure save_file method to separate metadata computation

--- src/file_handling/file_save.py
+++ src/file_handling/file_save.py
@@ -23,18 +23,15 @@ class FileSave:
     self.unique_filename = f"{self.uuid_str}{self.file_extension}"
     os.makedirs(self.STORAGE_DIR, exist_ok=True)
 
-  def save_file(self) -> dict:
+  def __compute_metadata(self, file_path: Path) -> dict:
     """
-    Saves the uploaded file to disk, computes metadata, and stores metadata in the database. 
+    Computes metadata for a given file path.
 
+    Args:
+        file_path: Path to the file for which to compute metadata.
     Returns:
-        metadata: metadata dictionary.
+        metadata: dictionary containing file metadata.
     """
-    # Save uploaded file
-    file_path = Path(self.STORAGE_DIR) / self.unique_filename
-    with file_path.open("wb") as buffer:
-      shutil.copyfileobj(self.file.file, buffer)
-    # Compute file metadata
     file_size = file_path.stat().st_size
     sha256_checksum = hashlib.sha256(file_path.read_bytes()).hexdigest()
     
@@ -50,7 +47,6 @@ class FileSave:
       if media_type is None:
         media_type = "application/octet-stream"
     
-    # Store metadata in DB
     metadata = {
       "id": self.uuid_str,
       "storage_path": str(file_path),
@@ -61,7 +57,24 @@ class FileSave:
       "sha256_checksum": sha256_checksum,
       "stored_as": self.unique_filename
     }
+    return metadata
+
+  def save_file(self) -> dict:
+    """
+    Saves the uploaded file to disk, computes metadata, and stores metadata in the database. 
+
+    Returns:
+        metadata: metadata dictionary.
+    """
+    # Save uploaded file
+    file_path = Path(self.STORAGE_DIR) / self.unique_filename
+    with file_path.open("wb") as buffer:
+      shutil.copyfileobj(self.file.file, buffer)
+    
+    # Compute & save file metadata
+    metadata = self.__compute_metadata(file_path)
     self.db.insert_file_metadata(metadata)
 
     # Return metadata for response to user
     return metadata
+    
